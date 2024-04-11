356029c4bfb98385d5628394819ed1dbbaa9bff8
Katsuhiro Ueno <uenobk@gmail.com>
2024-04-11 17:56:47 +0900

    lint .yaml files at commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 export default {
   '*.{js,cjs,jsx}': ['prettier -c', 'eslint'],
   '*.ts': ['prettier -c', () => 'tsc', 'eslint', () => 'vitest run'],
-  '*.json': ['prettier -c'],
+  '*.{json,yaml}': ['prettier -c'],
   '*.css': ['prettier -c', 'stylelint'],
   '*.{svelte,vue}': ['prettier -c', 'eslint', 'stylelint']
 }
