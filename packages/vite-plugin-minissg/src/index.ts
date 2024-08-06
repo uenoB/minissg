9a24ce8068da26f336fe4effc2a868588eb4043c
Katsuhiro Ueno <uenobk@gmail.com>
2024-08-06 09:15:54 +0900

    fix: export type Json

--- packages/vite-plugin-minissg/src/index.ts
+++ packages/vite-plugin-minissg/src/index.ts
@@ -5,7 +5,7 @@ import { buildPlugin } from './build'
 import { serverPlugin } from './server'
 export type { Options, Renderer } from './options'
 export type { Content, Module, ModuleName, Context, Main } from './module'
-export type { Awaitable } from './util'
+export type { Awaitable, Json } from './util'
 
 export default function (userOptions?: Options | undefined): PluginOption {
   const options = resolveOptions(userOptions)
